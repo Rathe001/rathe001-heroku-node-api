e37eb7f17f89eda954bc54908b88db26cadac62a
Josh Tummel <josh@astigmapro.com>
2020-04-17 11:54:25 -0400

    new server

--- src/server.js
+++ src/server.js
@@ -1,31 +1,52 @@
-import express from 'express';
+import WebSocket from 'websocket';
 import http from 'http';
-import ws from 'ws';
 
-let messages = [];
-const app = express();
-const server = http.createServer(app);
-const wss = new ws.Server({ server });
+const WebSocketServer = WebSocket.server;
 
-wss.on('connection', (ws) => {
-  ws.on('message', (data) => {
-    const { name, input, type } = JSON.parse(data);
-
-    switch (type) {
-      case 'connection':
-        messages.push({ name, input, type, id: messages.length + 1 });
-        break;
-      case 'message':
-      default:
-        messages.push({ name, input, type, id: messages.length + 1 });
-        break;
-    }
-    wss.clients.forEach((client) => {
-      ws.send(JSON.stringify(messages));
-    });
-  });
+var server = http.createServer(function (request, response) {
+  console.log(new Date() + ' Received request for ' + request.url);
+  response.writeHead(404);
+  response.end();
+});
+server.listen(8080, function () {
+  console.log(new Date() + ' Server is listening on port 8080');
 });
 
-server.listen(process.env.PORT || 3001, () => {
-  console.log(`Server started on port ${server.address().port}!`);
+const wsServer = new WebSocketServer({
+  httpServer: server,
+  // You should not use autoAcceptConnections for production
+  // applications, as it defeats all standard cross-origin protection
+  // facilities built into the protocol and the browser.  You should
+  // *always* verify the connection's origin and decide whether or not
+  // to accept it.
+  autoAcceptConnections: false,
+});
+
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed.
+  return true;
+}
+
+wsServer.on('request', function (request) {
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin
+    request.reject();
+    console.log(new Date() + ' Connection from origin ' + request.origin + ' rejected.');
+    return;
+  }
+
+  var connection = request.accept('echo-protocol', request.origin);
+  console.log(new Date() + ' Connection accepted.');
+  connection.on('message', function (message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ' + message.utf8Data);
+      connection.sendUTF(message.utf8Data);
+    } else if (message.type === 'binary') {
+      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+      connection.sendBytes(message.binaryData);
+    }
+  });
+  connection.on('close', function (reasonCode, description) {
+    console.log(new Date() + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
 });
